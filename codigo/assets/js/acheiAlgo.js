4c2f4f715ab07aeb3d98d3d337d069eb62898d84
Igor Barroso Almeida <igorba867@gmail.com>
2024-05-08 21:06:44 -0300

    começo cadastro de itens

--- codigo/assets/js/acheiAlgo.js
+++ codigo/assets/js/acheiAlgo.js
@@ -9,14 +9,40 @@ function preenche_select(req, select) {
     });
 }
 
-function cadastra_item() {
+/*
+function valida_cadastro_item(nome, tel, localizacao) {
+    
+}
+*/
 
+function encontra_instituicao(id, req) {
+    for (let i = 0; i < req.instituicoes.length; i++) {
+        if (req.instituicoes[i].id == id) {
+            return req.instituicoes[i];
+        }
+    }
+    // Caso não encontre a instituicao
+    return null;
+}
+
+function cadastra_item(req) {
+    // Variaveis que serão utilizadas
+    let nome = document.getElementById("nome_item").value;
+    let contato = document.getElementById("contato_item").value;
+    let local = document.getElementById("local_item").value;
+    // Procura a instituição
+    let instituicao = encontra_instituicao(document.getElementById("select_instituicao").value, req);
+
+    // Cria o novo objeto de item
+    let novo_item = {
+        id
+    }
 }
 
 // Declara variaveis
 var form_cadastro = document.getElementById("cadastro_item");
 var lista_input = document.getElementsByClassName("item_input");
-var caminho_JSON = "../assets/json/instituicao.json"
+var caminho_JSON = "../assets/json/instituicao.json";
 var requisicao = new XMLHttpRequest();
 
 // Leitura dos dados do json
@@ -30,8 +56,9 @@ requisicao.onload = function () {
     // Preenche o select de instituições
     let select = document.getElementById("select_instituicao");
     preenche_select(resposta_requisicao, select);
-};
 
-/*form_cadastro.addEventListener("submit", (event) => {
-    event.preventDefault();
-});*/
+    form_cadastro.addEventListener("submit", (event) => {
+        event.preventDefault();
+        cadastra_item(resposta_requisicao, lista_input);
+    });
+};
