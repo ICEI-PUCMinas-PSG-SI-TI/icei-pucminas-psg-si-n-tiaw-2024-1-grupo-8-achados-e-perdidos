9620716dadc0948c19dbe447ef4a8230a64439a8
Danilo Riguette <contato.danilo8201295@gmail.com>
2024-05-15 22:21:06 -0300

    Melhor estilização dos cards da main

--- codigo/assets/js/main.js
+++ codigo/assets/js/main.js
@@ -1,30 +1,41 @@
 /* JavaScript geral do projeto */
-import {carregarJSON} from "./modules/json.js";
+import { carregarJSON } from "./modules/json.js";
 
-let instituicoes = await carregarJSON('./assets/json/instituicao.json')
+let json = await carregarJSON('./assets/json/instituicao.json')
 let instituicoesEl = document.getElementById('instituicoes')
 
-if(instituicoes.instituicoes != undefined && instituicoes.instituicoes.length > 0)
-  instituicoesEl.innerHTML = ''
+/**
+ * Carrega os cards com as instituições
+ */
+Inicializar()
 
-instituicoes.instituicoes.map((el, index) => {
-  instituicoesEl.innerHTML += `
-    <div class="col d-flex justify-content-center">
-    <div class="card card-instituicao" href="instituicao.html" id="instituicao-${el.id}">
-      <div class="card-body">
-        <img
-          src="${el.link_img}"
-          class="item_img" alt="imagem do item perido">
-        <div>
-          <div class="card-title text-center w-100">${el.nome}</div>
-          <p class="fs-5 endereco text-start">${el.endereco}</p>
-          <p class="fs-5 bairro-cidade">${el.cidade}</p>
+function Inicializar() {
+  // Verifica se encontrou instituições
+  if (json.instituicoes != undefined && json.instituicoes.length > 0)
+    instituicoesEl.innerHTML = ''
+  else
+    alert('Não foram encontrados instituições!')
+
+  // Preenche o HTML com os conteúdos do banco
+  json.instituicoes.map((el, index) => {
+    instituicoesEl.innerHTML += 
+      `<div class="col mb-3 d-flex justify-content-center">
+        <div class="card card-instituicao" href="instituicao.html" id="instituicao-${el.id}">
+          <div class="card-body">
+            <img
+              src="${el.link_img}"
+              class="item_img" alt="imagem do item perido">
+            <div class="">
+              <div class="card-title text-center w-100 bg-white p-1 rounded-bottom rounded-4 border border-2">${el.nome}</div>
+              <p class="fs-5 endereco text-start">${el.endereco}</p>
+              <p class="fs-5 bairro-cidade">${el.cidade}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    </div>`
-  // Redireciona para um item ID
-  instituicoesEl.children[0].addEventListener('click', (el) => {
-    window.location.href = 'http://localhost:8080/codigo/pages/itensPerdidos.html?id='+el.id
-  })
-});
+      </div>`
+    // Redireciona para um item ID
+    instituicoesEl.children[0].addEventListener('click', (el) => {
+      window.location.href = './pages/itensPerdidos.html?id=' + el.id
+    })
+  });
+}
