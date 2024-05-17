c787e4ee271c2b1640eaaa60bea21f4074bcb3b3
Igor Barroso Almeida <igorba867@gmail.com>
2024-05-16 21:23:56 -0300

    criação de funções para manipulação do JSON

--- codigo/assets/js/acheiAlgo.js
+++ codigo/assets/js/acheiAlgo.js
@@ -1,12 +1,12 @@
 /* Arquivo de Javascript da página de cadastro de um item */
+//import {putJSON, getJSON} from "./modules/json.js";
 
 
-/* ----------- Funções ----------- */
+/* ----------- Funções inicio ----------- */
 function preenche_select(id, nome, select) {
     let text = `<option value="${id}">${nome}</option>`;
     select.innerHTML += text;
 }
-
 
 function encontra_instituicao(id, intituicoes) {
     for (let i = 0; i < intituicoes.length; i++) {
@@ -18,10 +18,9 @@ function encontra_instituicao(id, intituicoes) {
     return null;
 }
 
-
 function cadastra_item(intituicoes) {
     // Procura a instituição
-    let id_instituicao =document.getElementById("select_instituicao").value;
+    let id_instituicao = document.getElementById("select_instituicao").value;
     let instituicao = encontra_instituicao(id_instituicao, intituicoes);
 
     // To-do: alerta cadastro errado
@@ -76,71 +75,76 @@ function cadastra_item(intituicoes) {
         }
     }
 }
+/* ----------- Funções fim ----------- */
 
-// Declara variaveis
-var form_cadastro = document.getElementById("cadastro_item");
-var lista_input = document.getElementsByClassName("item_input");
-var div_img = document.getElementsByClassName("form_upload_img")[0];
-var link_preenchido = false;
-const caminho_JSON = "https://7632dd34-2094-462f-97e8-638cefefbbfe-00-xy9ocks2w8wk.riker.replit.dev/";
-var requisicao = new XMLHttpRequest();
-var link_img = null;
-var tipo_tags = [
-    "Eletrônico",
-    "Camisa",
-    "Calça",
-    "Acessório",
-    "Livro",
-    "Caderno",
-    "Outro"
-]
+function main() {
+    // Declara variaveis
+    var form_cadastro = document.getElementById("cadastro_item");
+    var lista_input = document.getElementsByClassName("item_input");
+    var div_img = document.getElementsByClassName("form_upload_img")[0];
+    var link_preenchido = false;
+    const caminho_JSON = "https://7632dd34-2094-462f-97e8-638cefefbbfe-00-xy9ocks2w8wk.riker.replit.dev/";
+    var requisicao = new XMLHttpRequest();
+    var link_img = null;
+    var tipo_tags = [
+        "Eletrônico",
+        "Camisa",
+        "Calça",
+        "Acessório",
+        "Livro",
+        "Caderno",
+        "Outro"
+    ]
 
-// Esse trecho de código implementa a função do input de imagem 
-div_img.addEventListener("click", () => {
-    link_img = prompt("Digite o link da imagem:");
-    if(link_img !== null) {
-        var img = new Image();
-        
-        // Para saber se o link é válido
-        img.src = link_img;
-        img.onload = function() {
-            div_img.innerHTML = `<img src="${link_img}" id="img_uploaded" alt="Imagem não carregou">`;
-            document.getElementById("img_uploaded").src = link_img;
-            link_preenchido = true;
-        };
-        img.onerror = function() {
-            alert("Link incorreto");
-            link_preenchido = false;
-        };
-    }
-});
-
-// Leitura dos dados do json
-requisicao.open("GET", caminho_JSON + "instituicoes");
-requisicao.responseType = "json";
-requisicao.send();
-
-requisicao.onload = function () {
-    let resposta_requisicao = requisicao.response;
-
-    // Preenche o select de instituições
-    let select_instituicao = document.getElementById("select_instituicao");
-    resposta_requisicao.forEach(element => {
-        preenche_select(element.id, element.nome, select_instituicao);
-    });
-
-    // Preenche o select de tags
-    let select_tag = document.getElementById("select_tag");
-    for(let i = 0; i < tipo_tags.length; i++) {
-        preenche_select(i, tipo_tags[i], select_tag)
-    }
-
-    form_cadastro.addEventListener("submit", (event) => {
-        event.preventDefault();
-        if(link_preenchido) {
-            cadastra_item(resposta_requisicao);
-        } else {
-            alert("O link da imagem está vazio ou não é valido");
+    // Esse trecho de código implementa a função do input de imagem 
+    div_img.addEventListener("click", () => {
+        link_img = prompt("Digite o link da imagem:");
+        if(link_img !== null) {
+            var img = new Image();
+            
+            // Para saber se o link é válido
+            img.src = link_img;
+            img.onload = function() {
+                div_img.innerHTML = `<img src="${link_img}" id="img_uploaded" alt="Imagem não carregou">`;
+                document.getElementById("img_uploaded").src = link_img;
+                link_preenchido = true;
+            };
+            img.onerror = function() {
+                alert("Link incorreto");
+                link_preenchido = false;
+            };
         }
     });
-};
+
+    // Leitura dos dados do json
+    requisicao.open("GET", caminho_JSON + "instituicoes");
+    requisicao.responseType = "json";
+    requisicao.send();
+
+    requisicao.onload = function () {
+        let resposta_requisicao = requisicao.response;
+
+        // Preenche o select de instituições
+        let select_instituicao = document.getElementById("select_instituicao");
+        resposta_requisicao.forEach(element => {
+            preenche_select(element.id, element.nome, select_instituicao);
+        });
+
+        // Preenche o select de tags
+        let select_tag = document.getElementById("select_tag");
+        for(let i = 0; i < tipo_tags.length; i++) {
+            preenche_select(i, tipo_tags[i], select_tag)
+        }
+
+        form_cadastro.addEventListener("submit", (event) => {
+            event.preventDefault();
+            if(link_preenchido) {
+                cadastra_item(resposta_requisicao);
+            } else {
+                alert("O link da imagem está vazio ou não é valido");
+            }
+        });
+    };
+}
+
+main();
--- codigo/assets/js/modules/json.js
+++ codigo/assets/js/modules/json.js
@@ -11,6 +11,47 @@ async function carregarJSON(caminhoArquivo) {
   }
 }
 
+// Função que define o onreadystatechange de uma requisição para capturar erros
+function setRequisicaoState(req) {
+  req.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            console.log('Resposta recebida:', xhr.responseText);
+        } else {
+            console.error('Erro na requisição:', xhr.status, xhr.statusText);
+        }
+    }
+    return req;
+};
+}
 
+// Recebe caminho da requisição e um valor no formato de objeto
+function putJSON(caminho, data) {
+  let req = new XMLHttpRequest();
+  req.open("PUT", caminho);
+  req = setRequisicaoState(req);
+  req.setRequestHeader("Content-Type", "application/json");
+  req.send(JSON.stringify(data));
+}
 
-export {carregarJSON}
+// Recebe caminho da requisição e um valor no formato de objeto
+function postJSON(caminho, data) {
+  let req = new XMLHttpRequest();
+  req.open("POST", caminho);
+  req = setRequisicaoState(req);
+  req.setRequestHeader("Content-Type", "application/json");
+  req.send(JSON.stringify(data));
+}
+
+// Recebe o caminho da requisição e retorna o json no formato de objeto
+function getJSON(caminho) {
+  let req = new XMLHttpRequest();
+  req.open("GET", caminho);
+  req = setRequisicaoState(req);
+  req.send();
+  req.onload = function() {
+    return JSON.parse(req.response);
+  }
+}
+
+export {carregarJSON, putJSON, getJSON, postJSON}
